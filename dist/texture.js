acc081289c9ffbb47bf0a236e6859c517a3103f8
Will Nayes <wnayes@gmail.com>
2018-08-15 22:17:38 -0500

    Remove unmaintained individual files.

--- dist/texture.js
+++
@@ -1,9 +0,0 @@
-import { WrappingMode } from "./types";
-var Texture = /** @class */ (function () {
-    function Texture() {
-        this.wrapS = WrappingMode.CLAMP_TO_EDGE;
-        this.wrapT = WrappingMode.CLAMP_TO_EDGE;
-    }
-    return Texture;
-}());
-export { Texture };
